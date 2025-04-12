38443ab9273939a91d15abb99c21efb21dcf3e7c
Matias Ancona <anconarmatias@gmail.com>
2025-04-11 22:13:22 -0300

    agrego archivos

---
+++ src/main/java/com/oo2/grupo24/Grupo24Application.java
@@ -0,0 +1,13 @@
+package com.oo2.grupo24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Grupo24Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/oo2/grupo24/Grupo24ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oo2.grupo24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
